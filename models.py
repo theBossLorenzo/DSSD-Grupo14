79a667adcbb2a329fe92a250071ebdbfee1ba0e2
lorenzohandula@gmail.com <lorenzinho98>
2021-10-18 18:26:32 -0300

    go back to commit a6cd9d2ea4d59864cd556844f19a9523636584cb

--- models.py
+++ models.py
@@ -16,7 +16,7 @@ class Sociedad(db.Model):
     comentario = db.Column(db.String())
     caseId = db.Column(db.Integer)
 
-    def __init__(self, nombre, estatuto, fecha_creacion, domicilio_legal, domicilio_real, representante, correo):
+    def __init__(self, nombre,estatuto,fecha_creacion,domicilio_legal,domicilio_real,representante,correo):
         self.nombre = nombre
         self.estatuto = estatuto
         self.fecha_creacion = fecha_creacion
@@ -24,6 +24,7 @@ class Sociedad(db.Model):
         self.domicilio_legal = domicilio_legal
         self.representante = representante
         self.correo = correo
+
 
     def __repr__(self):
         return '<id {}>'.format(self.id)
@@ -40,8 +41,8 @@ class Sociedad(db.Model):
             'correo': self.correo
         }
 
-
 class Socio(db.Model):
+
     __tablename__ = 'socio'
 
     id = db.Column(db.Integer, primary_key=True)
@@ -75,27 +76,24 @@ class Permiso(db.Model):
     nombre = db.Column(db.String(255))
     enabled = db.Column(db.Integer)
 
-    def __init__(self, nombre):
-        self.nombre = nombre
+    def __init__(self,nombre):
+        self.nombre=nombre
         self.enabled = 1
 
-
 association_table = db.Table('rol_tiene_permiso',
-                             db.Column('rol_id', db.Integer, db.ForeignKey('rol.id'), primary_key=True),
-                             db.Column('permiso_id', db.Integer, db.ForeignKey('permiso.id'), primary_key=True))
-
+                            db.Column('rol_id', db.Integer, db.ForeignKey('rol.id'), primary_key=True),
+                            db.Column('permiso_id', db.Integer, db.ForeignKey('permiso.id'), primary_key=True))
 
 class Rol(db.Model):
     __tablename__ = 'rol'
     id = db.Column(db.Integer, primary_key=True)
     nombre = db.Column(db.String(255))
     enabled = db.Column(db.Integer)
-    permisos = db.relationship("Permiso", secondary=association_table, lazy='subquery',
-                               backref=db.backref('rol', lazy=True))
+    permisos = db.relationship("Permiso",secondary=association_table,lazy='subquery', backref=db.backref('rol', lazy=True))
 
-    def __init__(self, nombre):
-        self.nombre = nombre
-        self.enabled = 1
+    def __init__(self,nombre):
+        self.nombre=nombre
+        self.enabled=1
 
     def buscarNombre(nombre):
         rol = Rol.query.filter_by(nombre=nombre)
