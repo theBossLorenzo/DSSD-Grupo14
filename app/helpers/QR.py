9db9092598921b669f7814cfc2ace20bc72898e4
lorenzohandula@gmail.com <lorenzinho98>
2021-11-04 18:13:58 -0300

    Generación de qr (ruta /pruebaQR). Por el momento con un nombre fijo, se almacenan en app/static/qr

--- app/helpers/QR.py
+++ app/helpers/QR.py
@@ -1,13 +1,8 @@
 import qrcode
-from PIL import Image
-import base64
 
 def generarQR():
-    imagen = qrcode.make('https://www.youtube.com/watch?v=tpEBRdmMpXE')
 
-    nombre_imagen = 'QR.png'
-    archivo_imagen = open(nombre_imagen, 'wb')
-    imagen.save(archivo_imagen)
-    archivo_imagen.close()
+    img = qrcode.make('https://www.youtube.com/watch?v=tpEBRdmMpXE')
+    img.save('app/static/qr/qr1.png')
 
     return True
